99d1e7877e77508714d45e19f8b566e64ef4da51
felipeperaltacheung <f33___@hotmail.com>
2022-10-29 03:46:08 -0300

    let

---
+++ SEC2-Basico/let/index.js
@@ -0,0 +1,29 @@
+// console.log("John born in 1982");
+// console.log("in 2000 John met Mary");
+// console.log("John married Mary in 2012");
+// console.log("Mary had a child with John in 2015");
+// console.log("John's son called Edward");
+
+let name = 'Jhon'; //String
+
+console.log(name, "born in 1982");
+console.log('in 2000', name, "met Mary");
+console.log(name, "married Mary in 2012");
+console.log("Mary had a child with", name, "in 2015");
+console.log(name, "'s son called Edward");
+
+let name2; //declaracao de variavel - mas estado de undefined
+name2 = 'Ana brown' // inicializando
+console.log(name2); // 'Ana brown'
+name2 = 'Joao blue' //modificando o valor da variavel
+console.log(name2); // 'Joao blue'
+
+//----------------------------------------------------------------
+
+//Nao pode criar variaveis com palavar reservada -> Ex: let if;
+//Variaveis precisam ter nomes significativos -> Ex: let nameClient;
+//Nao pode comecar o nome de variavel com um numero -> Ex: let 1name;
+//Nao pode conter espacor ou tracos -> Ex: let name-Cliente ou name Client;
+//Utilizar camelCase -> Ex: let fullName;
+//Case-sensitive
+
