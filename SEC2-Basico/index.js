e662c6b351c10a86015d47b742e1587ccde7b230
felipeperaltacheung <f33___@hotmail.com>
2022-10-29 02:19:18 -0300

    7-console

---
+++ SEC2-Basico/index.js
@@ -0,0 +1,6 @@
+console.log("The 'quick'"); // The 'quick'
+console.log('brown "fox"'); //  brown "fox"
+console.log(`'jumps' "over"`); // 'jumps' "over"
+
+console.log(23, 18.12, 'the quick'); //int
+
