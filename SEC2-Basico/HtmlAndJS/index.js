b2179ef18d7b2aefeee7fd99f12a0082efb0133a
felipeperaltacheung <f33___@hotmail.com>
2022-10-29 02:58:31 -0300

    html+js

---
+++ SEC2-Basico/HtmlAndJS/index.js
@@ -0,0 +1,9 @@
+//Mesclar JS com HTML
+
+
+console.log("teste")
+/*Code runner ->
+[Running] node "/home/felipe.cheung/ProjetosEstudos/udemy
+/FullStackLuis/SEC2-Basico/HtmlAndJS/tempCodeRunnerFile.js"
+*/
+
