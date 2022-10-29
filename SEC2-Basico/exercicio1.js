0a085f8284d31fb4a35251d91c4dc9477215f559
felipeperaltacheung <f33___@hotmail.com>
2022-10-29 02:30:26 -0300

    8-console_exercicio.

---
+++ SEC2-Basico/exercicio1.js
@@ -0,0 +1,3 @@
+// My name is "NAME". Iam learning Js at 10 AM.
+
+console.log('My name is "F". Iam learning Js at', 10, "AM." )
--- SEC2-Basico/index.js
+++ SEC2-Basico/index.js
@@ -2,5 +2,6 @@ console.log("The 'quick'"); // The 'quick'
 console.log('brown "fox"'); //  brown "fox"
 console.log(`'jumps' "over"`); // 'jumps' "over"
 
-console.log(23, 18.12, 'the quick'); //int
+console.log(23, 18.12, 'the quick'); //23 18.12 the quick
+// tudo e number
 
